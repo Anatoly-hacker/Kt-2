30d1764e80b84c44da4678827a3a7857bf619f66
Anatoly-hacker <157773160+Anatoly-hacker@users.noreply.github.com>
2024-04-03 22:56:05 +0500

    Update Task3.java

--- Кт-2/Task3.java
+++ Кт-2/Task3.java
@@ -2,22 +2,29 @@ import java.util.Arrays;
 
 public class Task3 {
     public static void main(String[] args) {
-        int[] nums = {1, 3,  3, 2, 4, 5, 3 };
+        int[] nums = {1, 1, 5,   2 };
         Arrays.sort(nums);
         int count = 1;
         int maxcount = 1;
         int element = nums[0];
+        boolean twoElement = false;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] == nums[i - 1]) {
                 count++;
                 if (count > maxcount) {
                     maxcount = count;
                     element = nums[i];
+                    twoElement = false;
+                } else if (count == maxcount) {
+                    twoElement = true;
                 }
             } else
                 count = 1;
-
         }
-        System.out.println(element);
+        if(twoElement)
+            System.out.println(-1);
+        else {
+            System.out.println(element);
+        }
     }
 }
